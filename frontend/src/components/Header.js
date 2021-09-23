3917d996cfae030e7670170d47f8b010bf90753b
Carlito Algarin <carlito1113@yahoo.com>
2021-09-23 00:38:13 -0400

    set up react router and adjusted header padding

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+      <Navbar className='p-4' bg="light" variant="light" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">E-Commerce Place</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">E-Commerce Place</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart"> <i className="fas fa-shopping-cart"/> Cart</Nav.Link>
-              <Nav.Link href="/login"> <i className="fas fa-user"/> Sign In</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  {' '}
+                  <i className="fas fa-shopping-cart" /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  {' '}
+                  <i className="fas fa-user" /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
