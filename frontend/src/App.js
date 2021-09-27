9be68aa5badf06e4e37d7f0caaec18297ed091f9
Carlito Algarin <carlito1113@yahoo.com>
2021-09-27 18:47:22 -0400

    created the cart component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <Route exact path='/' component={HomeScreen} />
-          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
